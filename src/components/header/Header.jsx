121f1b780d6ad9befe2e6ce475433ba2ffd8b20c
vnelai <118484488+vnelai@users.noreply.github.com>
2025-01-19 18:33:36 -0500

    Update css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,22 +4,24 @@ import './Header.css';
 function Header({toggleDarkMode}) {
   return (
     <header>
-        {/* Logo top left of page */}
-        <div className='logo-name'>
+        {/* Left Section: Logo / Name */}
+        <div className='header-left'>
             <a href="/">Virginia</a>
         </div>
 
         {/* Menu items */}
-        <nav className='menu'>
-            <ul>
-                <li><a href="/about">About</a></li>
-                <li><a href="/projects"></a></li>
-                <li><a href="/blog"></a></li>
-                <li><a href="/contact"></a></li>
-            </ul>
-        </nav>
+        <div className='menu-container'>
+            <nav className='menu'>
+                <ul>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/projects"></a></li>
+                    <li><a href="/blog"></a></li>
+                    <li><a href="/contact"></a></li>
+                </ul>
+            </nav>
+        </div>
 
-        {/* Light/Dark mode toggle */}
+        {/* Right Section: Mode Toggle */}
         <div className='mode-toggle'>
             <button onClick={toggleDarkMode}>🌙/🌞</button>
         </div>
