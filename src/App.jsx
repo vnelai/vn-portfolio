c3f6a30278d2a8d0496224a74cf2e6bf1ec6fead
vnelai <118484488+vnelai@users.noreply.github.com>
2025-01-19 23:05:13 -0500

    Sync the style changes between elements when mide changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/header/Header';
 import '@fortawesome/fontawesome-free/css/all.min.css';
@@ -7,13 +7,25 @@ function App() {
   // useState to track changes in mode-toggle
   const [isDarkMode, setDarkMode] = useState(false); 
 
+  // Effect to toggle the class on the body
+  useEffect(() => {
+    if (isDarkMode) {
+      document.body.classList.add('dark');
+      document.body.classList.remove('light');
+    } else {
+      document.body.classList.add('light');
+      document.body.classList.remove('dark');
+    }
+  }, [isDarkMode]);
+
   // Function to change the state of mode-toggle
   const toggleDarkMode = () => {
-    setDarkMode(!isDarkMode);
-  }
+    setDarkMode(prevMode => !isDarkMode);
+
+  };
 
   return (
-    <div className={isDarkMode ? 'dark' : 'light'}>
+    <div > 
       <Header toggleDarkMode={toggleDarkMode}/>
 
     </div>
