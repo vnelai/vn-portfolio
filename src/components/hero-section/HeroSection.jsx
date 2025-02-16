64a3c5c1121bd1ed34f3def57945a82dcee16816
vnelai <118484488+vnelai@users.noreply.github.com>
2025-02-16 18:46:13 -0500

    Add Social media links on Hero Section

--- src/components/hero-section/HeroSection.jsx
+++ src/components/hero-section/HeroSection.jsx
@@ -26,6 +26,32 @@ function HeroSection() {
               </a>
               <a href="/projects" className='hero-btn'>View Projects</a>
             </div>
+            <div class="social-media-div">
+              <a href="https://github.com/vnelai" class="sm-button github" target="_blank" rel="noopener noreferrer">
+                  <i class="fa-brands fa-github"></i>
+              </a> 
+              <a href="https://www.linkedin.com/in/virginianelai/" class="sm-button linkedin" target="_blank" rel="noopener noreferrer">
+                  <i class="fa-brands fa-linkedin-in"></i>
+              </a>
+              <a href="https://www.youtube.com/@StarlightLabsX" class="sm-button youtube" target="_blank" rel="noopener noreferrer">
+                  <i class="fa-brands fa-youtube"></i>
+              </a> 
+              <a href="https://www.facebook.com/StarlightLabsX/" class="sm-button facebook" target="_blank" rel="noopener noreferrer">
+                  <i class="fa-brands fa-facebook-f"></i>
+              </a>
+              <a href="https://www.instagram.com/starlightlabsx/" class="sm-button instagram" target="_blank" rel="noopener noreferrer">
+                  <i class="fa-brands fa-instagram"></i>
+              </a>
+              <a href="https://x.com/StarlightLabs_X" class="sm-button twitter" target="_blank" rel="noopener noreferrer">
+                  <i class="fa-brands fa-twitter"></i>
+              </a>
+              <a href="mailto:vnelai01@gmail.com" class="sm-button google" target="_blank" rel="noopener noreferrer">
+                  <i class="fa-brands fa-google"></i>
+              </a>
+              {/* <a href="https://medium.com" class="sm-button medium" target="_blank" rel="noopener noreferrer">
+                  <i class="fa-brands fa-medium"></i>
+              </a> */}
+          </div>
         </div>
     </section>
   );
