b8e24ee46662424214a7a30eeaadab17d548ba87
vnelai <118484488+vnelai@users.noreply.github.com>
2025-02-16 19:55:30 -0500

    Set up [ages and routes for the pages on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';  //Import React and React hooks
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+// Import Pages and global components
 import Header from './components/header/Header';
-import HeroSection from './components/hero-section/HeroSection';
+import Home from './pages/Home';
+import AboutMe from './pages/AboutMe';
+import Blog from './pages/Blog';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+// Import Styling
+import './App.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 function App() {
   // useState to track changes in mode-toggle
-  const [isDarkMode, setDarkMode] = useState(false); 
+  const [isDarkMode, setDarkMode] = useState(false); // I'm starting with lights on, because it's a better feeling turning lights off instead of turning lights on for users lol
 
   // Effect to toggle the class on the body
   useEffect(() => {
@@ -26,12 +34,19 @@ function App() {
   };
 
   return (
-    <div > 
-      <Header toggleDarkMode={toggleDarkMode}/>
-      <HeroSection/>
-
-    </div>
-      
+    <>
+      <Header toggleDarkMode={toggleDarkMode} />
+      <Router>
+        <Routes>
+          {/* Set up Routes for each page */}
+          <Route path="/" element={<Home />} />
+          <Route path="/about-me" element={<AboutMe />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeroSection from '../components/hero-section/HeroSection';
+
+function Home() {
+  return (
+    <div>
+        <HeroSection />
+    </div>
+  );
+}
+
+export default Home;
