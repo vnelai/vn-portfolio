aacc1768b27bf256f3f30b9e40f66388f55a597d
vnelai <118484488+vnelai@users.noreply.github.com>
2025-02-16 19:58:59 -0500

    Fixed the about link on header to link to path about-me

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -13,7 +13,7 @@ function Header({toggleDarkMode, isDarkMode}) {
         <div className='menu-container'>
             <nav className='menu'>
                 <ul>
-                    <li><a href="/about">About</a></li>
+                    <li><a href="/about-me">About</a></li>
                     <li><a href="/projects">Projects</a></li>
                     <li><a href="/blog">Blog</a></li>
                     <li><a href="/contact">Contact</a></li>
