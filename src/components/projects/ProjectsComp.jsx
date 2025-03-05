f1de621c57f0a1a5bd75d60cff85711f5ba8f1fb
vnelai <118484488+vnelai@users.noreply.github.com>
2025-03-05 12:44:51 -0500

    Refactored ProjectsComp to accept projects as a prop for reusability and flexibility

--- src/components/projects/ProjectsComp.jsx
+++ src/components/projects/ProjectsComp.jsx
@@ -1,24 +1,7 @@
 import React from 'react';
 import './ProjectsComp.css';
 
-function ProjectsComp() {
-    const projects = [
-        {
-          title: 'Project 1',
-          description: 'A short description of project 1.',
-          liveLink: 'https://project1.com',
-          repoLink: 'https://github.com/vnelai01/project1',
-          image: '/public/project1.img', // Placeholder image URL
-        },
-        {
-          title: 'Project 2',
-          description: 'A short description of project 2.',
-          liveLink: 'https://project2.com',
-          repoLink: 'https://github.com/vnelai01/project1',
-          image: '/public/project2.img',
-        },
-        // Add more projects as needed
-      ];
+function ProjectsComp({ projects }) { 
   return (
     <section className='projects-component'>
         <h1>My Projects</h1>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import ProjectsComp from '../components/projects/ProjectsComp';
 
+const projects = [
+  {
+    title: 'Project 1',
+    description: 'A short description of project 1.',
+    liveLink: 'https://project1.com',
+    repoLink: 'https://github.com/vnelai01/project1',
+    image: '/public/project1.img', // Placeholder image URL
+  },
+  {
+    title: 'Project 2',
+    description: 'A short description of project 2.',
+    liveLink: 'https://project2.com',
+    repoLink: 'https://github.com/vnelai01/project1',
+    image: '/public/project2.img',
+  },
+  // Add more projects as needed
+];
+
 function Projects() {
   return (
     <div>
-      <ProjectsComp/>
+      <ProjectsComp projects={projects}/>;
     </div>
   );
 }
