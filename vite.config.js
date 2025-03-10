582a2d52a352d855f100776042dee59e5a3e1e79
vnelai <118484488+vnelai@users.noreply.github.com>
2025-03-10 13:00:53 -0400

    Fixed routing issue by adding SPA support for Vercel deployment.

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // Ensure Vercel looks for the 'build' directory
+    outDir: 'dist',
   },
   server: {
     mimeTypes: {
-      '.pdf': 'application/pdf', // Explicitly set MIME type for PDF
+      '.pdf': 'application/pdf',
     },
   },
+  base: '/',
 });
