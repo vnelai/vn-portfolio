e4b146b2de00f480dc35bd55e79fdd93f00512b4
vnelai <118484488+vnelai@users.noreply.github.com>
2025-03-10 01:18:57 -0400

    Fixed relative paths of hero section images, and the package.json build commands

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // Ensure Vercel looks for the 'build' directory
+  },
   server: {
     mimeTypes: {
       '.pdf': 'application/pdf', // Explicitly set MIME type for PDF
