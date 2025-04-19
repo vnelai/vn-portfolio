478b8aa929508807a8ff25581a2f4cc6b44c3e18
vnelai <118484488+vnelai@users.noreply.github.com>
2025-04-19 02:39:32 -0400

    Enhance featured content styling and update project titles with emojis for better visibility

--- src/components/home-page-components/featured-content/FeaturedContent.jsx
+++ src/components/home-page-components/featured-content/FeaturedContent.jsx
@@ -15,7 +15,7 @@ function FeaturedContent() {
         <div className="featured-teaser">
           <img src="/Meal_Prep_Pro.png" alt="Project Thumbnail" />
           <div className='featured-text'>
-            <h2>Featured Project: Meal Prep Pro</h2>
+            <h2>🚀 Featured Project: Meal Prep Pro</h2>
             <p>Meal Prep Pro streamlines your meal planning with recipe search, favorites, and smart shopping lists. Built with React and Node.js for seamless cooking.</p>
             <a href="https://meal-prep-pro-fe.onrender.com" className="featured-link">View Project</a>
           </div>
@@ -23,7 +23,7 @@ function FeaturedContent() {
         <div className="featured-teaser">
           <img src="/Meal_Prep_Pro.png" alt="Project Thumbnail" />
           <div className='featured-text'>
-          <h2>Featured Blog: Blog Title....</h2>
+          <h2>📝 Featured Blog: Blog Title....</h2>
           <p>Blog teaser...</p>
           <a href="https://vnelai.github.io/React_Todo_List/" className="featured-link">View Post</a>
           </div>
