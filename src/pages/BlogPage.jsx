059342d9f53c33114b6bec1300c665ad02388caa
vnelai <118484488+vnelai@users.noreply.github.com>
2025-04-20 02:04:19 -0400

    Updated blog post images and fixed file path/case issues

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -5,9 +5,9 @@ import FeaturedBlogs from '../components/blog-components/featured-blogs/Featured
 import BlogSection from '../components/blog-components/blog-section/BlogSection';
 
 const articles = [
-  { id: 1, title: 'How to Build a Secure Web App', excerpt: 'In this post, we will explore...', date: 'March 10, 2025', image: 'link-to-image.jpg' },
-  { id: 2, title: 'Exploring Machine Learning in JavaScript', excerpt: 'This article covers how to integrate...', date: 'March 5, 2025', image: 'link-to-image.jpg' },
-  { id: 3, title: 'The Future of Cloud Computing', excerpt: 'In this post, we dive deep into...', date: 'February 28, 2025', image: 'link-to-image.jpg' },
+  { id: 1, title: 'How to Build a Secure Web App', excerpt: 'In this post, we will explore...', date: 'March 10, 2025', image: 'security.jpg' },
+  { id: 2, title: 'Exploring Machine Learning in JavaScript', excerpt: 'This article covers how to integrate...', date: 'March 5, 2025', image: 'ML.jpg' },
+  { id: 3, title: 'The Future of Cloud Computing', excerpt: 'In this post, we dive deep into...', date: 'February 28, 2025', image: 'CLOUD.jpg' }
   // Add more articles as needed
 ];
 
